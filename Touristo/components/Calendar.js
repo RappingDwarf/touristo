0305756272944986956d5f9716ccc764899585dd
RappingDwarf <daniel.wiesenfeld@gmx.de>
2023-06-21 19:01:44 +0200

    Calendar Update

--- Touristo/components/Calendar.js
+++ Touristo/components/Calendar.js
@@ -19,14 +19,28 @@ export default function DatePicker({ setStartDate, setEndDate }) {
     }
   }
 
+  const generateDateRange = (start, end) => {
+    const dates = {};
+    let currentDate = new Date(start);
+    const endDate = new Date(end);
+    while (currentDate <= endDate) {
+      dates[currentDate.toISOString().split('T')[0]] = { color: '#0085FF' };
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dates;
+  }
+
   return (
-    <Calendar
-      markingType={'period'}
-      onDayPress={onDayPress}
-      markedDates={{
-        [startDateLocal]: { startingDay: true, color: 'green' },
-        [endDateLocal]: { endingDay: true, color: 'green' },
-      }}
-    />
+
+      <Calendar
+        markingType={'period'}
+        onDayPress={onDayPress}
+        markedDates={{
+          ...generateDateRange(startDateLocal, endDateLocal),
+          [startDateLocal]: { startingDay: true, color: '#0085FF' },
+          [endDateLocal]: { endingDay: true, color: '#0085FF' },
+        }}
+      />
+
   );
 };
